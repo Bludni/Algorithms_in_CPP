b12b1a6a3bcb1f37c8fe80f8ec8b77dba49444ef
Bludni <elukej@gmail.com>
2022-02-03 09:01:14 +0100

    max cont sum revelation!

--- ctci16/ctci16.17.cpp
+++ ctci16/ctci16.17.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
-int lConstSum(int arr[], int size)
+// O(N^2) solution, without small modification, that i somehow managed to miss for a long time
+int lContSum(int arr[], int size)
 {
     int sum = arr[0];
     int max_sum = arr[0];
@@ -16,10 +17,25 @@ int lConstSum(int arr[], int size)
     return max_sum;
 }
 
+// O(N) time solution, beautiful solution, cleaner code, cleaner algorithm
+int lContSumFast(int arr[], int size)
+{
+    //We go through array tracking sum of continious elements. Whenever this sum
+    //becomes negative, it means this whole sequence that we passed can only
+    //contribute negatively to the sum of the sequences after so we discard it.
+    //this way in only one pass we get the maximum sum of continious elements
+    int sum = 0, max_sum = 0;
+    for (int i = 0; i < size; i++) {
+        sum = (sum + arr[i] > 0)? sum + arr[i] : 0;
+        max_sum = (sum > max_sum)? sum : max_sum;
+    }
+    return max_sum;
+}
+
 int main()
 {
     int size = 6;
     int arr[] ={2, -8, 20, -2, 4, -10};
-    std::cout << "Largest continious sum: "
-     << lConstSum(arr, size) << std::endl;
+    std::cout << "Largest continious sum faster way: "
+     << lContSumFast(arr, size) << std::endl;
 }
